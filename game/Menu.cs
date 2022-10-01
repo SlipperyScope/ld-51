a7a8c22b9e14a402f796c0c86ac0f6615e6d5991
Michael Lange <dingoeatingfuzz@gmail.com>
2022-10-01 10:06:43 -0700

    Make menu less bad

--- game/Menu.cs
+++ game/Menu.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class Menu : Node2D
+public class Menu : Control
 {
     // Declare member variables here. Examples:
     // private int a = 2;
@@ -16,7 +16,7 @@ public class Menu : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        string P = "CanvasLayer/PanelContainer/HBoxContainer/VBoxContainer/";
+        string P = "VBoxContainer/";
         GetNode<Button>(P + "Play").Connect("pressed", this, nameof(GotoScene), new Godot.Collections.Array { GameScene });
         GetNode<Button>(P + "Instructions").Connect("pressed", this, nameof(GotoScene), new Godot.Collections.Array { InstructionsScene });
         GetNode<Button>(P + "Exit").Connect("pressed", this, nameof(Exit));
