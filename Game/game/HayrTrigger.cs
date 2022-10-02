44209bf482fba163dc70259f00d528762434182b
Andrew <pXn.Tilde@protonmail.com>
2022-10-02 03:22:43 -0700

    triggers

---
+++ Game/game/HayrTrigger.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ld51.game
+{
+    public class HayrTrigger : RigidBody2D
+    {
+        [Export]
+        private List<NodePath> Triggerables = new();
+
+        public override void _EnterTree()
+        {
+            GetNode<Area2D>("Trigger").Connect("body_entered", this, nameof(OnBodyEntered));
+        }
+
+        private void OnBodyEntered(PhysicsBody2D body)
+        {
+            if (body is Arrow arrow)
+            {
+                foreach (var path in Triggerables)
+                {
+                    if (GetNode(path) is ISnowflake triggerable)
+                    triggerable.Trigger();
+                }
+            }
+        }
+    }
+}
---
+++ Game/game/Triggerator9000.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ld51.game
+{
+    public class Triggerator9000 : Node, ISnowflake
+    {
+        [Export]
+        private NodePath Target;
+
+        public void Trigger()
+        {
+            if (Target is null)
+            {
+                GD.PushWarning("Target is null");
+            }
+            else if (GetNode<Prop>(Target) is Prop prop)
+            {
+                prop.Enable();
+                prop.ApplyCentralImpulse(Vector2.Down);
+            }
+        }
+    }
+}
