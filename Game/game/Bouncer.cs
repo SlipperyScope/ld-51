9c9d86720c83f3daf1a8df981b7da03b60844d20
Victor Rossi <victor@nebulacore.net>
2022-10-02 16:58:53 -0700

    L E V E L S

--- Game/game/Bouncer.cs
+++ Game/game/Bouncer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,20 +7,20 @@ using Godot;
 
 namespace ld51.game
 {
-    public class Bouncer : StaticBody2D, IAudioTriggerable
-    {
-        private AudioStreamPlayer SFX;
-        public override void _Ready()
-        {
-            SFX = GetNode<AudioStreamPlayer>("SFX");
-        }
+	public class Bouncer : StaticBody2D, IAudioTriggerable
+	{
+		private AudioStreamPlayer SFX;
+		public override void _Ready()
+		{
+			SFX = GetNode<AudioStreamPlayer>("SFX");
+		}
 
-        public void TriggerAudio(Int32 index = 0)
-        {
-            switch (index)
-            {
-                case 0: SFX.Play(); break;
-            }
-        }
-    }
+		public void TriggerAudio(Int32 index = 0)
+		{
+			switch (index)
+			{
+				case 0: SFX.Play(); break;
+			}
+		}
+	}
 }
