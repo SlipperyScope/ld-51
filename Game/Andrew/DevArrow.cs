923a1d1884ba1a14562bebcf19bb1d7c37e85e58
Victor Rossi <victor@nebulacore.net>
2022-10-01 21:52:43 -0700

    Test Scene

--- Game/Andrew/DevArrow.cs
+++ Game/Andrew/DevArrow.cs
@@ -4,61 +4,61 @@ using System;
 //TODO: change to kinematic because godot physics sucks
 public class DevArrow : RigidBody2D
 {
-    public override void _EnterTree()
-    {
-        Connect("body_entered", this, nameof(OnBodyEntered));
-    }
+	public override void _EnterTree()
+	{
+		Connect("body_entered", this, nameof(OnBodyEntered));
+	}
 
-    public override void _Ready()
-    {
-        ApplyCentralImpulse(Transform.x * 5000f);
-    }
+	public override void _Ready()
+	{
+		ApplyCentralImpulse(Transform.x * 2000f);
+	}
 
-    public override void _PhysicsProcess(Single delta)
-    {
-        LookAt(GlobalPosition + LinearVelocity);
-    }
+	public override void _PhysicsProcess(Single delta)
+	{
+		LookAt(GlobalPosition + LinearVelocity);
+	}
 
-    public override void _IntegrateForces(Physics2DDirectBodyState state)
-    {
-        AppliedTorque = 0f;
-        AppliedForce = Vector2.Zero;
+	public override void _IntegrateForces(Physics2DDirectBodyState state)
+	{
+		AppliedTorque = 0f;
+		AppliedForce = Vector2.Zero;
 
 
-        base._IntegrateForces(state);
-    }
+		base._IntegrateForces(state);
+	}
 
-    private void OnBodyEntered(PhysicsBody2D body)
-    {
-        if (LinearVelocity.Length() < 100f)
-        {
-            CallDeferred(nameof(DisablePhysics));
-            // wiggle or something? 
-        }
+	private void OnBodyEntered(PhysicsBody2D body)
+	{
+		if (LinearVelocity.Length() < 100f)
+		{
+			CallDeferred(nameof(DisablePhysics));
+			// wiggle or something? 
+		}
 
-        switch (body)
-        {
-            case DevApple apple:
-                CallDeferred(nameof(DisablePhysics));
-                apple.AddDecal(GetNode<Sprite>("Sprite"));
-                //apple.ApplyImpulse(GlobalPosition - apple.GlobalPosition, LinearVelocity * Mass / apple.Mass);
-                //apple.Bonk(GlobalPosition, LinearVelocity * Mass);
-                //apple.CallDeferred(nameof(apple.Bonk), GlobalPosition, LinearVelocity * Mass);
-                apple.Bonk();
-                QueueFree();
-                break;
+		switch (body)
+		{
+			case DevApple apple:
+				CallDeferred(nameof(DisablePhysics));
+				apple.AddDecal(GetNode<Sprite>("Sprite"));
+				//apple.ApplyImpulse(GlobalPosition - apple.GlobalPosition, LinearVelocity * Mass / apple.Mass);
+				//apple.Bonk(GlobalPosition, LinearVelocity * Mass);
+				//apple.CallDeferred(nameof(apple.Bonk), GlobalPosition, LinearVelocity * Mass);
+				apple.Bonk();
+				QueueFree();
+				break;
 
-            default:
-                break;
-        }
-    }
+			default:
+				break;
+		}
+	}
 
-    private void DisablePhysics()
-    {
-        Mode = ModeEnum.Kinematic;
-        CollisionLayer = 0;
-        CollisionMask = 0;
-        ContactMonitor = false;
-        CustomIntegrator = true;
-    }
+	private void DisablePhysics()
+	{
+		Mode = ModeEnum.Kinematic;
+		CollisionLayer = 0;
+		CollisionMask = 0;
+		ContactMonitor = false;
+		CustomIntegrator = true;
+	}
 }
