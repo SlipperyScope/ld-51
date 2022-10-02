8f3f0c711ea30aaf0bd44326dddebcdf87e1495b
Michael Lange <dingoeatingfuzz@gmail.com>
2022-10-02 00:28:38 -0700

    Level sequencing

---
+++ Game/game/Global.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class Global : Node
+{
+    public static Game game;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+    public static void Win() {
+        if (game != null) {
+            game.NextLevel();
+        } else {
+            throw new Exception("Tried to win a level before seting game on Global");
+        }
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//
+//  }
+}
